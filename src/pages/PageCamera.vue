ae6d0b940383c15c53fadb9b1fac7e64793260d8
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2021-10-18 22:54:06 +0700

    get photo from camera

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,10 +1,22 @@
 <template>
   <q-page class="constrain-more q-pa-md">
     <div class="camera-frame q-pa-md">
-      <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <video v-show="!capturedImage" ref="video" class="full-width" autoplay />
+      <canvas
+        v-show="capturedImage"
+        ref="canvas"
+        class="full-width"
+        height="240"
+      />
     </div>
     <div class="text-center q-pa-md">
-      <q-btn color="grey-10" icon="eva-camera" size="lg" round />
+      <q-btn
+        @click="captureImage()"
+        color="grey-10"
+        icon="eva-camera"
+        size="lg"
+        round
+      />
     </div>
     <div class="row justify-center q-ma-md">
       <q-input
@@ -34,7 +46,7 @@
 
 <script>
 import { uid } from "quasar";
-
+require("md-gum-polyfill");
 export default {
   name: "PageCamera",
   data() {
@@ -46,7 +58,56 @@ export default {
         photo: null,
         date: Date.now(),
       },
+      capturedImage: false,
     };
+  },
+  methods: {
+    initCamera() {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true,
+        })
+        .then((stream) => {
+          this.$refs.video.srcObject = stream;
+        });
+    },
+    captureImage() {
+      let video = this.$refs.video;
+      let canvas = this.$refs.canvas;
+
+      canvas.width = video.getBoundingClientRect().width;
+      canvas.height = video.getBoundingClientRect().height;
+      let context = canvas.getContext("2d");
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      this.capturedImage = true;
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL());
+    },
+    dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = atob(dataURI.split(",")[1]);
+
+      // separate out the mime component
+      var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
+
+      // create a view into the buffer
+      var ia = new Uint8Array(ab);
+
+      // set the bytes of the buffer to the correct values
+      for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab], { type: mimeString });
+      return blob;
+    },
+  },
+  mounted() {
+    this.initCamera();
   },
 };
 </script>
